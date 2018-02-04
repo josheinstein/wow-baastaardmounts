bb181d3e53ac4c84bae33052a1a2077b39ccba45
Josh Einstein <josh@josheinstein.com>
2018-02-04 04:27:41 -0500

    Gutted the whole thing. Much cleaner.

--- Bstrd_Mounts/Bstrd_Mounts.lua
+++ Bstrd_Mounts/Bstrd_Mounts.lua
@@ -26,6 +26,7 @@ function BM:OnInitialize()
 
     -- Register /mountup slash command
     BM:RegisterChatCommand("mountup", "Handle_MountUp")                         -- calls up a mount for this zone
+    BM:RegisterChatCommand("debugmounts", "Handle_DebugMounts")                 -- lists the current situations and the mounts set for them
     BM:RegisterChatCommand("setmount", "Handle_SetMount")                       -- sets zone mount
     BM:RegisterChatCommand("setgroupmount", "Handle_SetGroupMount")				-- sets character's default mount when grouped
     BM:RegisterChatCommand("setgroundmount", "Handle_SetGroundMount")           -- sets character's default ground mount
@@ -36,172 +37,207 @@ function BM:OnInitialize()
 
 end
 
+function BM:GetCurrentSituation()
+
+    local continent = CurrentContinentName()
+    local zone = CurrentZoneName()
+    local situation = {}
+
+    if IsOutdoors() then
+    
+        -- if player is under level 20, always attempt to use the heirloom mount
+        if UnitLevel("player") < 20 then
+            
+            table.insert( situation, "Heirloom" )
+            
+        else
+
+            -- if player is grouped and a group mount is defined, that
+            -- will take precedence over the others
+            if IsInGroup() then
+                table.insert( situation, "Group" )
+            end
+
+            -- if a swimming mount is defined and we are swimming, that
+            -- will take priority over the other defined mounts
+            if IsSubmerged() then
+                if GetMirrorTimerInfo(2)=="BREATH" then
+                    table.insert( situation, "Underwater" )
+                end
+                table.insert( situation, "Swimming" )
+            end
+
+            -- Continent + Zone takes precedence over Continent + Default
+            table.insert( situation, MakeZoneKey(continent, zone) )
+            table.insert( situation, MakeZoneKey(continent) )
+
+            -- if there is no zone or continent specific mount, use a
+            -- default mounts for this character, ground or flying
+            if IsFlyableArea() then
+                table.insert( situation, "Flying" )
+            else
+                table.insert( situation, "Ground" )
+            end
+
+        end
+
+    else
+        table.insert( situation, "Indoors" )
+    end
+
+    return situation
+
+end
+
 -- Called when /mountup slash command is used
 function BM:Handle_MountUp() 
 
-    local mountName = nil
-    local continent = tostring(GetCurrentMapContinent()+0)
-    local zoneText = GetZoneText()
     local mountDB = self.db.profile
     
     if IsMounted() then
         
         Dismount()
         
-    elseif IsOutdoors() then
-    
-        -- if player is under level 20, always attempt to use the heirloom mount
-        if playerLevel < 20 then
-            
-            mountName = "Summon Chauffeur"
-            
-        else
-
-            -- if player is grouped and a group mount is defined, that
-            -- will take precedence over the others
-            if not mountName and IsInGroup() then mountName = s(mountDB["Group"]) end
-
-            -- if a swimming mount is defined and we are swimming, that
-            -- will take priority over the other defined mounts
-            if not mountName and IsSubmerged() then
-                if GetMirrorTimerInfo(2)=="BREATH" then
-                    mountName = s(mountDB["Underwater"])
-                else
-                    mountName = s(mountDB["Swimming"])                    
-                end
-            end
-
-            if mountDB[continent] then
-                -- try to use a zone-specific mount if one is configured
-                if not mountName then mountName = s(mountDB[continent][zoneText]) end
-                -- if there is no zone-specific mount, try a continent-specific mount
-                if not mountName then mountName = s(mountDB[continent]["Default"]) end
-            end
-
-            -- if there is no zone or continent specific mount, use a
-            -- default mounts for this character, ground or flying
-            if not mountName then
-                if IsFlyableArea() then
-                    mountName = s(mountDB["Flying"])
-                else
-                    mountName = s(mountDB["Ground"])
-                end
-            end
-            
-            if not mountName then
-                if playerClassName == "DRUID" and playerLevel >= 18 then
-                    mountName = "Travel Form"
-                end
+    else
+        
+        local situation = BM:GetCurrentSituation()
+        
+        for i,s in ipairs(situation) do
+            local mountName = mountDB[s]
+            if mountName then
+                BM:Printf("[%s] %s", s, mountName)
+                CastSpellByName(mountName)
+                return
             end
             
         end
 
+        BM:Printf("No preferred mounts set.")
+    
     end
 
-    if mountName then
+end
 
-        BM:Printf("[%s] %s: %s", continent, zoneText, mountName)
+-- Called when /debugmounts slash command is used
+function BM:Handle_DebugMounts()
 
-        CastSpellByName(mountName)
-        
+    local mountDB = self.db.profile
+    local situation = BM:GetCurrentSituation()
+    
+    BM:Printf("---------MOUNT SCENARIOS--------")    
+    
+    for i,s in ipairs(situation) do
+        local mountName = mountDB[s]
+        if mountName then
+            local spellName, spellRank, spellIcon, castingTime, minRange, maxRange, spellID = GetSpellInfo(mountName)
+            if spellName then
+                BM:Printf("When [%s] use [%i:%s]", s, spellID, spellName)
+            else
+                BM:Printf("When [%s] use [Unknown:%s]", s, mountName)
+            end
+        else
+            BM:Printf("When [%s] use [Not Specified]", s)
+        end
     end
 
+    BM:Printf("-------------------------------")    
+    
 end
 
 -- Called when /setgroupmount slash command is used
 function BM:Handle_SetGroupMount(mountName) 
-    BM:SetMount(nil, "Group", mountName)
+    BM:SetMount("Group", mountName)
 end
 
 -- Called when /setflyingmount slash command is used
 function BM:Handle_SetFlyingMount(mountName) 
-    BM:SetMount(nil, "Flying", mountName)
+    BM:SetMount("Flying", mountName)
 end
 
 -- Called when /setgroundmount slash command is used
 function BM:Handle_SetGroundMount(mountName) 
-    BM:SetMount(nil, "Ground", mountName)
+    BM:SetMount("Ground", mountName)
 end
 
 -- Called when /setonwatermount slash command is used
 function BM:Handle_SetOnWaterMount(mountName) 
-    BM:SetMount(nil, "Swimming", mountName)
+    BM:SetMount("Swimming", mountName)
 end
 
 -- Called when /setunderwatermount slash command is used
 function BM:Handle_SetUnderwaterMount(mountName) 
-    BM:SetMount(nil, "Underwater", mountName)
+    BM:SetMount("Underwater", mountName)
 end
 
 -- Called when /setcontinentmount slash command is used
 function BM:Handle_SetContinentMount(mountName) 
-    local continent = tostring(GetCurrentMapContinent()+0)
-    BM:SetMount(continent, "Default", mountName)
+    local continent = CurrentContinentName()
+    BM:SetMount(MakeZoneKey(continent), mountName)
 end
 
 -- Called when /setmount slash command is used
 function BM:Handle_SetMount(mountName) 
 
-    local continent = tostring(GetCurrentMapContinent()+0)
-    local zoneText = GetZoneText()
+    local continent = CurrentContinentName()
+    local zone = CurrentZoneName()
 
-    BM:SetMount(continent, zoneText, mountName)
+    BM:SetMount(MakeZoneKey(continent, zone), mountName)
 
 end
 
-function BM:SetMount(continent, zoneOrScenario, mountName) 
+function BM:SetMount(situation, mountName) 
 
     local mountDB = self.db.profile
 
-    continent = s(continent)
-    zoneOrScenario = s(zoneOrScenario)
-    mountName = s(mountName)
-
-    -- If a continent was specified, we will use that continent as the mountDB.
-    -- First make sure a table exists for the given continent. If not, create one.
-    if continent then
-        if not mountDB[continent] then
-            mountDB[continent] = {}
-        end
-        mountDB = mountDB[continent]
-    end
-
-    if not zoneOrScenario then
-        zoneOrScenario = "Default"
-    end
-
-    if mountName then
+    if not IsNullOrWhiteSpace(mountName) then
 
         -- make sure we can use the mount
         local spellName, spellRank, spellIcon, castingTime, minRange, maxRange, spellID = GetSpellInfo(mountName)
         if spellName then
             -- set up new mount mapping
-            mountDB[zoneOrScenario] = spellName
-            BM:Printf("Will use [%i] %s.", spellID, mountName)
+            mountDB[situation] = spellName
+            BM:Printf("When [%s] use [%i:%s].", situation, spellID, mountName)
         else
-            BM:Printf("Unknown mount: [%i] %s", spellID, mountName)
+            BM:Printf("Unknown mount: %s", mountName)
         end
 
     else
         -- clears out an existing mount mapping
-        mountName = s(mountDB[zoneOrScenario])
+        mountName = mountDB[situation]
         if mountName then
-            BM:Printf("Clearing zone mount: %s", mountName)
-            mountDB[zoneOrScenario] = nil
+            BM:Printf("Clearing [%s] mount preference. (Was [%s])", situation, mountName)
+            mountDB[situation] = nil
         end
     end
 
 end
 
--- cleans a string by trimming leading/trailing spaces and converting
--- empty strings to nil
-function s(str)
-    if str and str:len() > 0 then str = str:gsub("^%s*(.-)%s*$", "%1") end
-    if str and str:len() > 0 then
-        return str
+function CurrentContinentName()
+    return GetContinentName(GetCurrentMapContinent()) or "World"
+end
+
+function CurrentZoneName()
+    return GetZoneText()
+end
+
+function MakeZoneKey(continent, zone)
+    if continent and zone then
+        return continent .. ":" .. zone
+    elseif continent then
+        return continent
+    else
+        return "Unknown"
     end
 end
 
+function IsNullOrWhiteSpace(str)
+    if str and str:len() > 0 then str = str:gsub("^%s*(.-)%s*$", "%1") end
+    if str and str:len() > 0 then
+        return false
+    else
+        return true
+    end
+end
 
 -- Export global addon object which can be referenced by
 -- other Lua scripts in the addon.
